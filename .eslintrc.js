6898829a007d45da123acf4d7b621405c6a9eab4
Nihey Takizawa <nihey.takizawa@gmail.com>
2023-06-08 18:48:40 -0300

    Initial commit from Create Next App

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'react/no-unknown-property': [
+      2,
+      {
+        ignore: [
+          'jsx',
+          'global',
+        ],
+      },
+    ],
+  },
+}
