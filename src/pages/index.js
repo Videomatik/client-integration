7fb2d5e106f3db29fc1b2d2db86ca65b5b4b4911
renanblamas <renanblamas@gmail.com>
2023-06-12 19:26:55 -0300

    refactor: extract product info from a data parameter as JSON instead of multiple pameters

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
 // Example URL:
-// http://localhost:3000/?name=produto&image=https%3A%2F%2Fstorage.videomatik.com.br%2Fvideomatik%2Ftemplates%2Foferta-varejo-nujyuua%2Fassets%2Fespykfb8--compressed-png.png&price=1000&description=description%20wfwg.rewn%20whfwheoiw&templateId=oferta-varejo-nujyuua
+// http://localhost:3000/?data={"name":"produto","templateId":"oferta-varejo-nujyuua","price":"1000", "description":"description fhergmeru rufghnerhg", "image":"https://storage.videomatik.com.br/videomatik/templates/oferta-varejo-nujyuua/assets/espykfb8--compressed-png.png"}
 
+import { useMemo } from 'react'
 import { useRouter } from 'next/router'
 import PreviewPlayer from '../components/preview-player/PreviewPlayer'
 
 export default function Home() {
   const router = useRouter()
-  const productData = router.query
+  const { data } = router.query
 
-  if (!productData.name) {
-    return <div />
+  if (!data) {
+    return 'URL Parameter (Data) is missing'
   }
 
+  const dataJSON = useMemo(() => JSON.parse(data), [data])
   return (
     <div className="index-page">
       <PreviewPlayer
         templateId="oferta-varejo-nujyuua"
-        data={productData}
+        data={dataJSON}
       />
       <style jsx>
         {`
